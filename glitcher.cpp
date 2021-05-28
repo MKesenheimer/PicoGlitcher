d66a64f0652bb9b5db7147523ce73a35876e656f
MKesenheimer <mkesenheimer@gmx.net>
2021-05-28 23:16:17 +0200

    Umbenennungen

--- glitcher.cpp
+++ glitcher.cpp
@@ -12,54 +12,54 @@
 #include <iostream>
 #include "pico/stdlib.h"
 
-const uint PDND_GLITCH = 22;
-const uint PDND_GLITCH_ACTIVE = 26;
+const uint PINGLITCH = 22;
+const uint PINGLITCHACTIVE = 26;
 
-const char CMD_DELAY = 'd';
-const char CMD_PULSE = 'p';
-const char CMD_GLITCH = 'g';
+const char DELAY = 'd';
+const char PULSE = 'p';
+const char GLITCH = 'g';
 
 // GPIO Pin to read the Power Voltage from
-const int IN_NRF_VDD = 0;
+const int PINVDD = 0;
 
 // TODO
 static inline void power_cycle_target() {
-  while(!gpio_get(IN_NRF_VDD));
+  while(!gpio_get(PINVDD));
 }
 
 int main() {
   // init
   stdio_init_all();
-  gpio_init(PDND_GLITCH);
-  gpio_set_dir(PDND_GLITCH, GPIO_OUT);
-  gpio_init(PDND_GLITCH_ACTIVE);
-  gpio_set_dir(PDND_GLITCH_ACTIVE, GPIO_OUT);
+  gpio_init(PINGLITCH);
+  gpio_set_dir(PINGLITCH, GPIO_OUT);
+  gpio_init(PINGLITCHACTIVE);
+  gpio_set_dir(PINGLITCHACTIVE, GPIO_OUT);
   
   uint32_t delay, pulse;
   while(1) {
     uint8_t cmd = std::cin.get();
     switch (cmd) {
-      case CMD_DELAY:
+      case DELAY:
         std::cin >> delay;
         //std::cout << "setting delay to " << delay << std::endl;
         break;
-      case CMD_PULSE:
+      case PULSE:
         std::cin >> pulse;
         //std::cout << "setting pulse to " << pulse << std::endl;
         break;
-      case CMD_GLITCH:
+      case GLITCH:
         std::cout << "delay = " << delay << ", pulse = " << pulse << std::endl;
         //power_cycle_target();
-        gpio_put(PDND_GLITCH_ACTIVE, 1);
+        gpio_put(PINGLITCHACTIVE, 1);
         for (uint32_t i = 0; i < delay; ++i) {
           __asm__("NOP");
         }
-        gpio_put(PDND_GLITCH, 1);
+        gpio_put(PINGLITCH, 1);
         for (uint32_t i = 0; i < pulse; ++i) {
           __asm__("NOP");
         }
-        gpio_put(PDND_GLITCH_ACTIVE, 0);
-        gpio_put(PDND_GLITCH, 0);
+        gpio_put(PINGLITCHACTIVE, 0);
+        gpio_put(PINGLITCH, 0);
       default:
         break;
     }
