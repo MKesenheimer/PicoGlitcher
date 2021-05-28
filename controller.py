06dd527dcac308922f1f4d53fe0789ca542a48e4
MKesenheimer <mkesenheimer@gmx.net>
2021-05-28 22:04:46 +0200

    First commit of the Raspberry Pi voltage glitcher

---
+++ controller.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial
+import threading
+import time
+import argparse
+
+class listening(threading.Thread):
+  """listening class capable of threading."""
+
+  def __init__(self, ser):
+    """class initializer"""
+    self.__stopEvent = threading.Event()
+    self.__ser = ser
+    super().__init__()
+
+  def read(self):
+    """function to read data from the serial port"""
+    data = str("")
+    while self.__ser.in_waiting > 0:
+      payload = self.__ser.read(self.__ser.in_waiting)
+      data = payload.decode("ascii")
+    return data
+
+  def run(self):
+    """main thread"""
+    while not self.__stopEvent.isSet():
+      data = self.read()
+      print(data, end='')
+      time.sleep(0.1)
+
+  def join(self, timeout=None):
+    """set stop event and join within a given time period"""
+    self.__stopEvent.set()
+    print("stopping listening thread.")
+    super().join(timeout)
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Control the Glitcher over serial")
+  parser.add_argument('port', metavar='SERIALPORT', type=str, nargs=1,
+                      help='serial port to connect to')
+  parser.add_argument('--baudrate', dest='baudrate', type=int, nargs=1, default=[115200],
+                      help='sum the integers (default: find the max)')
+  parser.add_argument('--timeout', dest='timeout', type=float, nargs=1, default=[0.1],
+                      help='Timeout of the serial connection')
+  parser.add_argument('-d', dest='delay', type=int, nargs=3, default=[1,100,1],
+                      help='delay of the pulse')
+  parser.add_argument('-p', dest='pulse', type=int, nargs=3, default=[1,100,1],
+                      help='pulse width')
+  args = parser.parse_args()
+
+  ser = serial.Serial(port=args.port[0], baudrate=args.baudrate[0], timeout=args.timeout[0])
+
+  x = listening(ser)
+  x.start()
+  time.sleep(args.timeout[0])
+
+  try:
+    while True:
+      for d in range(args.delay[0], args.delay[1] + 1, args.delay[2]):
+        for p in range(args.pulse[0], args.pulse[1] + 1, args.pulse[2]):
+          time.sleep(args.timeout[0])
+          bts = bytes("d {}\n".format(d), "ascii")
+          ser.write(bts)
+          bts = bytes("p {}\n".format(p), "ascii")
+          ser.write(bts)
+          ser.write(b"g\n")
+          # TODO
+          #if testDevice(): # JTAG etc
+          #  print("SUCCESS")
+          #  exit(0)
+
+  except (KeyboardInterrupt, SystemExit):
+    x.join(args.timeout[0])
+    print("\nGood bye.")
+    exit(0)
---
+++ glitcher.cpp
@@ -0,0 +1,69 @@
+/*
+ * glitcher for Raspberry Pi Pico
+ *
+ * @version     1.0.0
+ * @author      Matthias Kesenheimer
+ * @copyright   2021
+ * @licence     MIT
+ *
+ */
+
+#include <stdio.h>
+#include <iostream>
+#include "pico/stdlib.h"
+
+const uint PDND_GLITCH = 22;
+const uint PDND_GLITCH_ACTIVE = 26;
+
+const char CMD_DELAY = 'd';
+const char CMD_PULSE = 'p';
+const char CMD_GLITCH = 'g';
+
+// GPIO Pin to read the Power Voltage from
+const int IN_NRF_VDD = 0;
+
+// TODO
+static inline void power_cycle_target() {
+  while(!gpio_get(IN_NRF_VDD));
+}
+
+int main() {
+  // init
+  stdio_init_all();
+  gpio_init(PDND_GLITCH);
+  gpio_set_dir(PDND_GLITCH, GPIO_OUT);
+  gpio_init(PDND_GLITCH_ACTIVE);
+  gpio_set_dir(PDND_GLITCH_ACTIVE, GPIO_OUT);
+  
+  uint32_t delay, pulse;
+  while(1) {
+    uint8_t cmd = std::cin.get();
+    switch (cmd) {
+      case CMD_DELAY:
+        std::cin >> delay;
+        //std::cout << "setting delay to " << delay << std::endl;
+        break;
+      case CMD_PULSE:
+        std::cin >> pulse;
+        //std::cout << "setting pulse to " << pulse << std::endl;
+        break;
+      case CMD_GLITCH:
+        std::cout << "delay = " << delay << ", pulse = " << pulse << std::endl;
+        //power_cycle_target();
+        gpio_put(PDND_GLITCH_ACTIVE, 1);
+        for (uint32_t i = 0; i < delay; ++i) {
+          __asm__("NOP");
+        }
+        gpio_put(PDND_GLITCH, 1);
+        for (uint32_t i = 0; i < pulse; ++i) {
+          __asm__("NOP");
+        }
+        gpio_put(PDND_GLITCH_ACTIVE, 0);
+        gpio_put(PDND_GLITCH, 0);
+      default:
+        break;
+    }
+  }
+
+  return 0;
+}
